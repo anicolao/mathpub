2b6da982165198b42e63fc948dba57cc99f3ff40
Alex Nicolaou <anicolao@gmail.com>
2026-07-18 10:24:16 -0400

    Revert "Add scale-checked physics validation examples"
    This reverts commit dfc6a427ada0b2be68e7677125d8a0c82e617c79.

--- src/mathpub/render.py
+++ src/mathpub/render.py
@@ -105,7 +105,6 @@ def document_tex(publication: dict[str, Any], projection: str, questions: list[s
 \setmathfont{{LibertinusMath-Regular.otf}}
 \usepackage[per-mode=symbol]{{siunitx}}
 \usepackage{{tikz,microtype}}
-\usetikzlibrary{{calc}}
 \setlength\parindent{{0pt}}
 {answers}
 \framedsolutions
